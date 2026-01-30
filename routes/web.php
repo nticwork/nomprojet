b7477378fe39478c95b5d1d0ebb1a782408f4d29
Ntic work <ntic.work@gmail.com>
2026-01-30 20:00:07 +0100

    add route

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,8 @@ dd(env('APP_KEY'));
 Route::get('/hello', function () {     
 return response('<h1>Hello From Vercel </H1>');
 });
+
+
+Route::get('/hello2', function () {     
+return response('<h1>Hello 2 From Vercel </H1>');
+});
